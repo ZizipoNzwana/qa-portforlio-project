dd24daecad8686d9c39e759eda9a51ba79c37076
Sibuyiselwe Zizipo Nzwana <sinzwjhb025@student.wethinkcode.co.za>
2026-06-30 22:12:24 +0200

    Created NUnit tests using C#

---
+++ UnitTests/Calculator.cs
@@ -0,0 +1,25 @@
+namespace UnitTests;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Division(int a, int b)
+    {
+        return a / b;
+    }
+
+    public int Multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿namespace UnitTests;
+
+public class Tests
+{
+    [Test]
+    public void Add_TwoAndThree_ReturnsFive()
+    {
+        // Arrange
+        var calculator = new Calculator();
+        int a = 2;
+        int b = 3;
+        int expected = 5;
+
+        // Act
+        int actual = calculator.Add(a, b);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+
+    public void SubstractingTenAndEight_ReturnsTwo()
+    {
+
+        var calculator = new Calculator();
+        int a = 10;
+        int b =  2;
+
+        int expected = 8;
+
+        int result = calculator.Subtract(a, b);
+        
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void multiply()
+    {
+        var calculator = new Calculator();
+        int a = 2;
+        int b = 4;
+
+        int expected = 8;
+
+        int result = calculator.Multiplication(a, b);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void divide()
+    {
+        var calculator = new Calculator();
+
+        int a = 10;
+        int b = 2;
+
+        int expected = 5;
+
+        int result = calculator.Division(a, b);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
